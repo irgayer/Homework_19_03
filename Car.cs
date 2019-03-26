14f1dddd609be945a85653ae25ddfd594e50cf67
YerbolIrgaliyev <yerbol.irgaliyev@gmail.com>
2019-03-26 06:56:08 +0600

    first commit

---
+++ Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_19_03
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public double MaxSpeed { get; set; }
+        public double Weight { get; set; }
+        public string Color { get; set; }
+        public int CountOfSeats { get; set; }
+        public int Cost { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name : {Name}");
+            Console.WriteLine($"Max speed : {MaxSpeed}Km/H");
+            Console.WriteLine($"Weight : {Weight}Kg");
+            Console.WriteLine($"Color : {Color}");
+            Console.WriteLine($"Count of Seats : {CountOfSeats}");
+            Console.WriteLine($"Cost : {Cost}$");
+        }
+        public void ChangeColor(string newColor)
+        {
+            if (!string.IsNullOrWhiteSpace(newColor))
+            {
+                Color = newColor;
+                Console.WriteLine("Color changed!");
+            }
+            else Console.WriteLine("Error");
+        }
+        public void UpgradeEngine(double plusSpeed)
+        {
+            if(plusSpeed >= 0)
+            {
+                MaxSpeed += plusSpeed;
+                Console.WriteLine("Engine upgraded");
+            }
+            else Console.WriteLine("Error");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Homework_19_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise1();
+            Console.WriteLine("------------------------");
+            Exercise2();
+        }
+        static void Exercise1()
+        {
+            Type t = typeof(Console);
+            var methodInfo = t.GetMethods();
+
+            foreach(var c in methodInfo)
+            {
+                var parametrInfo = c.GetParameters();
+                for(int i = 0; i < parametrInfo.Length; i++)
+                {
+                    Console.Write($"{parametrInfo[i].ParameterType.Name} {parametrInfo[0].Name}");
+                }
+                Console.WriteLine($"{c.ReturnType.Name} {c.Name}");
+            }
+        }
+        static void Exercise2()
+        {
+            Car car = new Car()
+            {
+                Name = "Ferrari 404",
+                MaxSpeed = 321,
+                Weight = 2500,
+                Color = "Red",
+                CountOfSeats = 2,
+                Cost = 250000
+            };
+            Type t = car.GetType();
+            foreach(var mi in t.GetMethods())
+            {
+                Console.WriteLine($"{mi.ReturnType.Name} {mi.Name}");
+            }
+            foreach(var pi in t.GetProperties())
+            {
+                Console.WriteLine($"{pi.PropertyType.Name} {pi.Name}");
+            }
+        }
+    }
+}
